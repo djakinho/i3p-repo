05ef2faeb2f4c337bc9093a263b529ab136e7843
djakinho <djakson.filho@gmail.com>
2023-11-10 20:29:07 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// document.getElementById("form").addEventListener("submit", function (event) {
+//   event.preventDefault();
+
+//   var nome = document.getElementById("nome").value;
+//   var email = document.getElementById("email").value;
+//   var mensagem = document.getElementById("mensagem").value;
+
+//   // Aqui você pode usar a lógica para enviar o formulário por e-mail ou realizar outras ações necessárias
+//   console.log("Nome: " + nome);
+//   console.log("E-mail: " + email);
+//   console.log("Mensagem: " + mensagem);
+
+//   // Limpa os campos do formulário após o envio
+//   document.getElementById("nome").value = "";
+//   document.getElementById("email").value = "";
+//   document.getElementById("mensagem").value = "";
+// });
